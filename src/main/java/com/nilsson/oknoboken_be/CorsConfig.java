f8764d93612733ff15ff1d3e5bbfb2374aacd8c4
Flipize <nilsson.filip@hotmail.se>
2025-02-02 02:44:50 +0100

    Update corsconf

--- src/main/java/com/nilsson/oknoboken_be/CorsConfig.java
+++ src/main/java/com/nilsson/oknoboken_be/CorsConfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@PropertySource("classpath:application.properties")
 public class CorsConfig {
 
     @Value("${frontend.url}")
